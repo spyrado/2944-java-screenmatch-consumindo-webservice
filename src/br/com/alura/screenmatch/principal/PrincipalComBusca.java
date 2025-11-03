3b6373f6d219457037f64400c6b6bab0adf7ef8b
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2025-11-03 08:42:23 -0300

    feat: buscando via input

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -4,13 +4,20 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
 
   public static void main(String[] args) {
+
+    Scanner leitura = new Scanner(System.in);
+    System.out.println("Digite um filme para busca: ");
+    var busca = leitura.nextLine();
+    String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=4f274003";
+
     HttpClient client = HttpClient.newHttpClient();
     HttpRequest request = HttpRequest.newBuilder()
-        .uri(URI.create("https://www.ofmdbapi.com/?i=tt3896198&apikey=4f274003"))
+        .uri(URI.create(endereco))
         .build();
     client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
         .thenApply(HttpResponse::body)
