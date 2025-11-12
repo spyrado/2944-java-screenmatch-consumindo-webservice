ed0bdc8439f7986d286d2c45c8e07afc563a8c9c
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2025-11-12 09:02:10 -0300

    feat: é ruim colocar pra pegar exception generica pois quando eu coloquei uma especifica e lancei ela mas nao mapiei essa especifica ela caiu na generica

--- src/br/com/alura/screenmatch/modelos/Titulo.java
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -1,91 +1,95 @@
 package br.com.alura.screenmatch.modelos;
 
+import br.com.alura.screenmatch.exceptions.ErroDeConversaoDeAnoException;
 import com.google.gson.annotations.SerializedName;
 
 public class Titulo implements Comparable<Titulo> {
-    private String nome;
-    private int anoDeLancamento;
-    private boolean incluidoNoPlano;
-    private double somaDasAvaliacoes;
-    private int totalDeAvaliacoes;
-    private int duracaoEmMinutos;
+  private String nome;
+  private int anoDeLancamento;
+  private boolean incluidoNoPlano;
+  private double somaDasAvaliacoes;
+  private int totalDeAvaliacoes;
+  private int duracaoEmMinutos;
 
-    public Titulo(String nome, int anoDeLancamento) {
-        this.nome = nome;
-        this.anoDeLancamento = anoDeLancamento;
+  public Titulo(String nome, int anoDeLancamento) {
+    this.nome = nome;
+    this.anoDeLancamento = anoDeLancamento;
+  }
+
+  public Titulo(TituloOmdb meuTituloOmdb) {
+    this.nome = meuTituloOmdb.title();
+    if (meuTituloOmdb.year().length() > 4) {
+      throw new ErroDeConversaoDeAnoException("Não consegui converter o ano porque tem mais de 04 caracteres.");
     }
-
-    public Titulo(TituloOmdb meuTituloOmdb) {
-        this.nome = meuTituloOmdb.title();
-        this.anoDeLancamento = Integer.parseInt(meuTituloOmdb.year());
+    this.anoDeLancamento = Integer.parseInt(meuTituloOmdb.year());
 //        this.duracaoEmMinutos = Integer.parseInt(meuTituloOmdb.runtime().split(" ")[0]);
-    }
+  }
 
-    public String getNome() {
-        return nome;
-    }
+  public String getNome() {
+    return nome;
+  }
 
-    public int getAnoDeLancamento() {
-        return anoDeLancamento;
-    }
+  public int getAnoDeLancamento() {
+    return anoDeLancamento;
+  }
 
-    public boolean isIncluidoNoPlano() {
-        return incluidoNoPlano;
-    }
+  public boolean isIncluidoNoPlano() {
+    return incluidoNoPlano;
+  }
 
-    public int getDuracaoEmMinutos() {
-        return duracaoEmMinutos;
-    }
+  public int getDuracaoEmMinutos() {
+    return duracaoEmMinutos;
+  }
 
-    public int getTotalDeAvaliacoes() {
-        return totalDeAvaliacoes;
-    }
+  public int getTotalDeAvaliacoes() {
+    return totalDeAvaliacoes;
+  }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 
-    public void setAnoDeLancamento(int anoDeLancamento) {
-        this.anoDeLancamento = anoDeLancamento;
-    }
+  public void setAnoDeLancamento(int anoDeLancamento) {
+    this.anoDeLancamento = anoDeLancamento;
+  }
 
-    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
-        this.incluidoNoPlano = incluidoNoPlano;
-    }
+  public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+    this.incluidoNoPlano = incluidoNoPlano;
+  }
 
-    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
-        this.duracaoEmMinutos = duracaoEmMinutos;
-    }
+  public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+    this.duracaoEmMinutos = duracaoEmMinutos;
+  }
 
-    public void exibeFichaTecnica(){
-        System.out.println("Nome do filme: " + nome);
-        System.out.println("Ano de lançamento: " + anoDeLancamento);
-    }
+  public void exibeFichaTecnica() {
+    System.out.println("Nome do filme: " + nome);
+    System.out.println("Ano de lançamento: " + anoDeLancamento);
+  }
 
-    public void avalia(double nota){
-        somaDasAvaliacoes += nota;
-        totalDeAvaliacoes++;
-    }
+  public void avalia(double nota) {
+    somaDasAvaliacoes += nota;
+    totalDeAvaliacoes++;
+  }
 
-    public double pegaMedia(){
-        return somaDasAvaliacoes / totalDeAvaliacoes;
-    }
+  public double pegaMedia() {
+    return somaDasAvaliacoes / totalDeAvaliacoes;
+  }
 
-    @Override
-    public int compareTo(Titulo outroTitulo) {
-        return this.getNome().compareTo(outroTitulo.getNome());
-    }
+  @Override
+  public int compareTo(Titulo outroTitulo) {
+    return this.getNome().compareTo(outroTitulo.getNome());
+  }
 
 
-    @Override
-    public String toString() {
-        return "Titulo{" +
-            "duracaoEmMinutos=" + duracaoEmMinutos +
-            ", totalDeAvaliacoes=" + totalDeAvaliacoes +
-            ", somaDasAvaliacoes=" + somaDasAvaliacoes +
-            ", incluidoNoPlano=" + incluidoNoPlano +
-            ", anoDeLancamento=" + anoDeLancamento +
-            ", nome='" + nome + '\'' +
-            '}';
-    }
+  @Override
+  public String toString() {
+    return "Titulo{" +
+        "duracaoEmMinutos=" + duracaoEmMinutos +
+        ", totalDeAvaliacoes=" + totalDeAvaliacoes +
+        ", somaDasAvaliacoes=" + somaDasAvaliacoes +
+        ", incluidoNoPlano=" + incluidoNoPlano +
+        ", anoDeLancamento=" + anoDeLancamento +
+        ", nome='" + nome + '\'' +
+        '}';
+  }
 }
