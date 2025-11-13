2f6fbda9f08624a7f8b8d7d43afd67a6502fccbb
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2025-11-12 22:24:25 -0300

    feat: aprendendo a utilizar FileWriter

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -7,6 +7,7 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -62,6 +63,11 @@ public class PrincipalComBusca {
 
       Titulo meuTitulo = new Titulo(meuTituloOmdb);
       System.out.println("meuTitulo: " + meuTitulo);
+
+      FileWriter escrever = new FileWriter("filme.txt");
+      escrever.write(meuTitulo.toString());
+      escrever.close();
+
     } catch (NumberFormatException e) {
       System.out.println("Aconteceu um erro: ");
       System.out.println(e.getMessage());
